28da9e3ac7989a030e8cd60a17b4b473d562f00b
Nakhim <cheanakhim.it@gmail.com>
2021-07-10 01:23:15 +0700

    update code footer

--- components/footer-rice.js
+++ components/footer-rice.js
@@ -3,9 +3,9 @@ import { getLocaletext } from "$/utils/localization";
 
 const FooterRice = () => {
   return (
-    <div class="grid grid-cols-6 p-14" style={{backgroundColor: "#252424"}}>
+    <div class="grid grid-cols-6 py-10" style={{backgroundColor: "#252424"}}>
       <div class="col-start-2 col-span-4 sm:col-start-1 sm:col-end-7  md:col-start-1 md:col-end-7  xl:col-start-2 xl:col-span-4">
-      <div className="grid gap-10 grid-cols-1 sm:grid-cols-4  text-white justify-items-center">
+      <div className="grid gap-10 sm:gap-0 grid-cols-1 sm:grid-cols-4  text-white justify-items-center">
       <div>
         <div className="text-center">
           <div>
