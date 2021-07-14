c7110e9a49042b534309fdf9c28aa3ee31ef7c20
nakhimchea <cheanakhim.it@gmail.com>
2021-07-14 23:11:37 +0700

    disabl rule

--- next.config.js
+++ next.config.js
@@ -22,4 +22,9 @@ module.exports = nextTranslate({
   experimental: {
     scrollRestoration: true,
   },
+  eslint: {
+    // Warning: Dangerously allow production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 });
